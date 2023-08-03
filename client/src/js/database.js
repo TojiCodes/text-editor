e3f722a2cdc53cd45b77b998210a7ddf79ea33fe
/\///\/_//\ _ <tojicodes@gmail.com>
2023-08-03 15:04:51 -0500

    updated database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,66 +1,47 @@
 import { openDB } from 'idb';
 
-const initdb = async () =>
-  openDB('jate', 1, {
+const DB_NAME = 'jate';
+const DB_VERSION = 1;
+const OBJECT_STORE_NAME = 'jate';
+
+const initDB = async () => {
+  return openDB(DB_NAME, DB_VERSION, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
+      if (!db.objectStoreNames.contains(OBJECT_STORE_NAME)) {
+        db.createObjectStore(OBJECT_STORE_NAME, { keyPath: 'id', autoIncrement: true });
+        console.log('jate database created');
+      } else {
         console.log('jate database already exists');
-        return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
     },
   });
+};
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+let db;
+const getDB = async () => {
+  if (!db) {
+    db = await initDB();
+  }
+  return db;
+};
+
 export const putDb = async (content) => {
-  try {
-    // Opens the database
-    const db = await initdb();
-    
-    // Opens a readwrite transaction
-    const tx = db.transaction('jate', 'readwrite');
-  
-    // Gets the notes object store
-    const store = tx.objectStore('jate');
-  
-    // Adds the note to the object store
-    await store.put(content);
-  
-    // Waits for the transaction to complete
-    await tx.done;
-
-    console.log('Data stored successfully');
-  } catch (error) {
-    console.error('Failed to store data:', error);
-  }
+  console.log('PUT to the database');
+  const jateDb = await getDB();
+  const tx = jateDb.transaction(OBJECT_STORE_NAME, 'readwrite');
+  const store = tx.objectStore(OBJECT_STORE_NAME);
+  const request = store.put({ jate: content });
+  await tx.done;
+  console.log('🚀 - data saved to the database');
 };
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  try {
-    // Opens the database
-    const db = await initdb();
-    
-    // Opens a readonly transaction
-    const tx = db.transaction('jate', 'readonly');
-  
-    // Gets the notes object store
-    const store = tx.objectStore('jate');
-  
-    // Gets all the notes from the object store
-    const data = await store.getAll();
-  
-    // Waits for the transaction to complete
-    await tx.done;
-    
-    console.log('Data retrieved successfully');
-  
-    // Returns the data
-    return data;
-  } catch (error) {
-    console.error('Failed to retrieve data:', error);
-  }
+  console.log('GET all from the database');
+  const jateDb = await getDB();
+  const tx = jateDb.transaction(OBJECT_STORE_NAME, 'readonly');
+  const store = tx.objectStore(OBJECT_STORE_NAME);
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
 };
-
-initdb();
