440a9d407f0526c5066ab713be4938d68a1d2e32
/\///\/_//\ _ <tojicodes@gmail.com>
2023-08-03 15:06:21 -0500

    updated editor.js

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -1,12 +1,12 @@
-// Import methods to save and get data from the indexedDB database in './database.js'
 import { getDb, putDb } from './database';
 import { header } from './header';
 
+const LOCAL_STORAGE_KEY = 'content';
+
 export default class {
   constructor() {
-    const localData = localStorage.getItem('content');
+    const localData = localStorage.getItem(LOCAL_STORAGE_KEY);
 
-    // check if CodeMirror is loaded
     if (typeof CodeMirror === 'undefined') {
       throw new Error('CodeMirror is not loaded');
     }
@@ -22,21 +22,20 @@ export default class {
       tabSize: 2,
     });
 
-    // When the editor is ready, set the value to whatever is stored in indexeddb.
-    // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
       console.info('Loaded data from IndexedDB, injecting into editor');
       this.editor.setValue(data || localData || header);
+    }).catch((err) => {
+      console.log(err);
     });
 
     this.editor.on('change', () => {
-      localStorage.setItem('content', this.editor.getValue());
+      localStorage.setItem(LOCAL_STORAGE_KEY, this.editor.getValue());
     });
 
-    // Save the content of the editor when the editor itself is loses focus
     this.editor.on('blur', () => {
       console.log('The editor has lost focus');
-      putDb(localStorage.getItem('content'));
+      putDb(this.editor.getValue());
     });
   }
 }
